d107ab5721b24d0ba3745b7096361ba91f277339
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2017-03-24 16:21:23 +0100

    Improve code organization

--- pymo.py
+++ pymo.py
@@ -1,174 +1,57 @@
-import sys, os, random, argparse
+import argparse, random
 from PIL import Image
-import numpy as np
 
+from pymo.mosaic import image_list, generate_photo_mosaic
 
-def image_list(image_dir):
-    images = []
-    image_files = os.listdir(image_dir)
-    for file in image_files:
-        path = os.path.abspath(os.path.join(image_dir, file))
-        try:
-            with open(path, "rb") as image_file:
-                image = Image.open(image_file)
-                image.load()
-                images.append(image)
-        except Exception as e:
-            print("Invalid image %s: %s" % (path, e))
-            continue
-
-    return images
-
-
-def average_rgb(image):
-    image_array = np.array(image)
-    w, h, d = image_array.shape
-    return tuple(np.average(image_array.reshape(w * h, d), axis=0))
-
-
-def tile_matrix(image, rows, cols):
-    w, h = int(image.size[0] / cols), int(image.size[1] / rows)
-    return [image.crop((i * w, j * h, (i + 1) * w, (j + 1) * h)) for j in range(rows) for i in range(cols)]
-
-
-def shortest_distance_index(average_rgb, average_rgbs):
-    avg = average_rgb
-    min_index = 0
-    min_dist = float("inf")
-    for i, val in enumerate(average_rgbs):
-        dist = ((val[0] - avg[0]) * (val[0] - avg[0]) +
-                (val[1] - avg[1]) * (val[1] - avg[1]) +
-                (val[2] - avg[2]) * (val[2] - avg[2]))
-        if dist < min_dist:
-            min_dist = dist
-            min_index = i
-    return min_index
-
-
-def create_image_grid(images, rows, cols):
-    # get the maximum height and width of the images
-    # don't assume they're all equal
-    width = max([img.size[0] for img in images])
-    height = max([img.size[1] for img in images])
-
-    grid_img = Image.new('RGB', (cols * width, rows * height))
-    # paste the tile images into the image grid
-    for i, image in enumerate(images):
-        row = int(i / cols)
-        col = i - cols * row
-        grid_img.paste(image, (col * width, row * height))
-
-    return grid_img
-
-
-def create_photomosaic(target_image, input_images, grid_rows, grid_cols, reuse_images=False):
-    # split the target image into tiles
-    target_images = tile_matrix(target_image, grid_rows, grid_cols)
-    # for each tile, pick one matching input image
-    output_images = []
-    # for user feedback
-    count = 0
-    batch_size = int(len(target_images) / 10)
-    # calculate the average of the input image
-    avgs = []
-    for img in input_images:
-        avgs.append(average_rgb(img))
-
-    for img in target_images:
-        # compute the average RGB value of the image
-        avg = average_rgb(img)
-        # find the matching index of closest RGB value # from a list of average RGB values
-        match_index = shortest_distance_index(avg, avgs)
-        output_images.append(input_images[match_index])
-        # user feedback
-        if count > 0 and batch_size > 10 and count % batch_size is 0:
-            print('processed %d of %d...' % (count, len(target_images)))
-        count += 1
-        # remove the selected image from input if flag set
-        if not reuse_images:
-            input_images.remove(match_index)
-    # create photomosaic image from tiles
-    mosaic_image = create_image_grid(output_images, grid_rows, grid_cols)
-    # display the mosaic
-    return mosaic_image
-
-
-def main():
-    # Command line args are in sys.argv[1], sys.argv[2] ..
-    # sys.argv[0] is the script name itself and can be ignored
-
-    # parse arguments
-    parser = argparse.ArgumentParser(description='Creates a photomosaic from input images')
-    # add arguments
-    parser.add_argument('--target-image', dest='target_image', required=True)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Creates a photo mosaic from input images')
+    parser.add_argument('--input-image', dest='input_image', required=True)
     parser.add_argument('--input-folder', dest='input_folder', required=True)
     parser.add_argument('--grid-rows', dest='grid_rows', required=True)
     parser.add_argument('--grid-cols', dest='grid_cols', required=True)
-    parser.add_argument('--output-file', dest='outfile', required=False)
+    parser.add_argument('--output', dest='output', required=True)
+    parser.add_argument('--strategy', dest='strategy', required=True, default='argb')
 
     args = parser.parse_args()
 
-    ###### INPUTS ######
-
-    # target image
-    target_image = Image.open(args.target_image)
-
-    # input images
+    target_image = Image.open(args.input_image)
     print('reading input folder...')
     input_images = image_list(args.input_folder)
 
-    # check if any valid input images found
-    if input_images == []:
-        print('No input images found in %s. Exiting.' % (args.input_folder,))
+    if not input_images:
+        print('No images found in %s. Exiting.' % args.input_folder)
         exit()
 
-    # shuffle list - to get a more varied output?
     random.shuffle(input_images)
 
-    # output
-    output_filename = 'mosaic.png'
-    if args.outfile:
-        output_filename = args.outfile
-
-    # resize the input to fit original image size?
+    # resize the input to fit original image size
     resize_input = True
     reuse_images = True
 
     grid_rows = int(args.grid_rows)
     grid_cols = int(args.grid_cols)
 
-    ##### END INPUTS #####
+    output = '%s.png' % args.output
 
-    print('starting photomosaic creation...')
+    # which algorithm to use
+    strategy = args.strategy
 
-    # if images can't be reused, ensure m*n <= num_of_images
+    print('Creating photo mosaic...')
+
     if not reuse_images:
         if grid_rows * grid_cols > len(input_images):
-            print('grid size less than number of images')
+            print('Insufficient number of images.')
             exit()
 
-    # resizing input
     if resize_input:
-        print('resizing images...')
-        # for given grid size, compute max dims w,h of tiles
+        print('Resizing images...')
         dims = (int(target_image.size[0] / grid_cols),
                 int(target_image.size[1] / grid_rows))
-        print("max tile dims: %s" % (dims,))
-        # resize
+
         for img in input_images:
             img.thumbnail(dims)
 
-    # create photomosaic
-    mosaic_image = create_photomosaic(target_image, input_images, grid_rows, grid_cols, reuse_images)
+    mosaic_image = generate_photo_mosaic(target_image, input_images, grid_rows, grid_cols, reuse_images)
+    mosaic_image.save(output, 'PNG')
 
-    # write out mosaic
-    mosaic_image.save('%s.png' % output_filename, 'PNG')
-
-    print("saved output to %s" % (output_filename,))
-    print('done.')
-
-
-# Standard boilerplate to call the main() function to begin
-# the program.
-if __name__ == '__main__':
-    main()
+    print("Saved mosaic to %s" % output)
---
+++ pymo/mosaic.py
@@ -0,0 +1,53 @@
+import os
+from PIL import Image
+
+from pymo.strategies import argb
+
+
+def image_list(image_dir):
+    images = []
+    image_files = os.listdir(image_dir)
+    for file in image_files:
+        path = os.path.abspath(os.path.join(image_dir, file))
+        try:
+            with open(path, "rb") as image_file:
+                image = Image.open(image_file)
+                image.load()
+                images.append(image)
+        except Exception as e:
+            print("Invalid image %s: %s" % (path, e))
+            continue
+
+    return images
+
+
+def tile_matrix(image, rows, cols):
+    w, h = int(image.size[0] / cols), int(image.size[1] / rows)
+    return [image.crop((i * w, j * h, (i + 1) * w, (j + 1) * h)) for j in range(rows) for i in range(cols)]
+
+
+def image_grid(images, rows, cols):
+    width = max([img.size[0] for img in images])
+    height = max([img.size[1] for img in images])
+
+    grid_img = Image.new('RGB', (cols * width, rows * height))
+    
+    for i, image in enumerate(images):
+        row = int(i / cols)
+        col = i - cols * row
+        grid_img.paste(image, (col * width, row * height))
+
+    return grid_img
+
+
+def generate_photo_mosaic(input_image, input_images, grid_rows, grid_cols, reuse_images=False, strategy=argb):
+    input_tiles = tile_matrix(input_image, grid_rows, grid_cols)
+    outputs = strategy(input_tiles, input_images, reuse_images)
+    mosaic_image = image_grid(outputs, grid_rows, grid_cols)
+
+    return mosaic_image
+
+
+
+
+
