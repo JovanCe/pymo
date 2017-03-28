cef0e1e6e518de3c1357d5474f151fc67eadaec2
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2017-03-28 17:26:55 +0200

    Replace linear with nearest neighbour search

--- pymo/strategies/argb.py
+++ pymo/strategies/argb.py
@@ -1,17 +1,5 @@
 import numpy as np
-
-
-def _shortest_distance_index(target_rgb, candidate_rgbs):
-    min_index = 0
-    min_dist = float("inf")
-    for i, val in enumerate(candidate_rgbs):
-        dist = ((val[0] - target_rgb[0]) * (val[0] - target_rgb[0]) +
-                (val[1] - target_rgb[1]) * (val[1] - target_rgb[1]) +
-                (val[2] - target_rgb[2]) * (val[2] - target_rgb[2]))
-        if dist < min_dist:
-            min_dist = dist
-            min_index = i
-    return min_index
+from sklearn.neighbors import KDTree
 
 
 def _average_rgb(image):
@@ -21,23 +9,22 @@ def _average_rgb(image):
 
 
 def argb(input_tiles, input_images, reuse_images=True):
-    avgs = []
+    candidates = []
     outputs = []
     input_length = len(input_tiles)
     batch_size = int(input_length / 10)
 
     for img in input_images:
-        avgs.append(_average_rgb(img))
+        candidates.append(_average_rgb(img))
+
+    rgb_tree = KDTree(np.array(candidates))
 
     for i, tile in enumerate(input_tiles):
-        avg = _average_rgb(tile)
-        match_index = _shortest_distance_index(avg, avgs)
-        outputs.append(input_images[match_index])
+        target = _average_rgb(tile)
+        match_index = rgb_tree.query(np.array(target).reshape(1, -1))
+        outputs.append(input_images[match_index[1][0][0]])
 
         if i % batch_size is 0:
             print('Processed %d of %d...' % (i, input_length))
-
-        if not reuse_images:
-            input_images.remove(match_index)
 
     return outputs
