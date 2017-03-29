1d1749baccb52aa1a50913f8aed344bedf2954c5
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2017-03-29 18:22:31 +0200

    Tweak rgb to lab function

--- pymo/strategies/alab.py
+++ pymo/strategies/alab.py
@@ -3,55 +3,32 @@ from sklearn.neighbors import KDTree
 
 
 def rgb_to_lab(input_rgb):
-    num = 0
-    RGB = [0, 0, 0]
+    rgb = [0, 0, 0]
 
-    for value in input_rgb:
+    for i, value in enumerate(input_rgb):
         value = float(value) / 255
-
         if value > 0.04045:
             value = ((value + 0.055) / 1.055) ** 2.4
         else:
             value /= 12.92
+        rgb[i] = value * 100
 
-        RGB[num] = value * 100
-        num += 1
+    xyz = []
+    # ref_X =  95.047   Observer= 2°, Illuminant= D65
+    xyz[0] = float(round(rgb[0] * 0.4124 + rgb[1] * 0.3576 + rgb[2] * 0.1805, 4)) / 95.047
+    # ref_Y = 100.000
+    xyz[1] = float(round(rgb[0] * 0.2126 + rgb[1] * 0.7152 + rgb[2] * 0.0722, 4)) / 100.0
+    # ref_Z = 108.883
+    xyz[2] = float(round(rgb[0] * 0.0193 + rgb[1] * 0.1192 + rgb[2] * 0.9505, 4)) / 108.883
 
-    XYZ = [0, 0, 0, ]
-
-    X = RGB[0] * 0.4124 + RGB[1] * 0.3576 + RGB[2] * 0.1805
-    Y = RGB[0] * 0.2126 + RGB[1] * 0.7152 + RGB[2] * 0.0722
-    Z = RGB[0] * 0.0193 + RGB[1] * 0.1192 + RGB[2] * 0.9505
-    XYZ[0] = round(X, 4)
-    XYZ[1] = round(Y, 4)
-    XYZ[2] = round(Z, 4)
-
-    XYZ[0] = float(XYZ[0]) / 95.047  # ref_X =  95.047   Observer= 2°, Illuminant= D65
-    XYZ[1] = float(XYZ[1]) / 100.0  # ref_Y = 100.000
-    XYZ[2] = float(XYZ[2]) / 108.883  # ref_Z = 108.883
-
-    num = 0
-    for value in XYZ:
-
+    for i, value in enumerate(xyz):
         if value > 0.008856:
             value **= 0.3333333333333333
         else:
             value = (7.787 * value) + (16 / 116)
+        xyz[i] = value
 
-        XYZ[num] = value
-        num += 1
-
-    Lab = [0, 0, 0]
-
-    L = (116 * XYZ[1]) - 16
-    a = 500 * (XYZ[0] - XYZ[1])
-    b = 200 * (XYZ[1] - XYZ[2])
-
-    Lab[0] = round(L, 4)
-    Lab[1] = round(a, 4)
-    Lab[2] = round(b, 4)
-
-    return Lab
+    return [round(116 * xyz[1] - 16, 4), round(500 * (xyz[0] - xyz[1]), 4), round(200 * (xyz[1] - xyz[2]), 4)]
 
 
 def _average_lab(image):
